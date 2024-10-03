3bb3db968933ac00f6ea3074d3895b8d0f37de89
Whitos <pavel.tsanev@lycee-faure.fr>
2024-10-03 10:24:29 +0200

    feat: Création de la classe Trticount

---
+++ src/Tricount.java
@@ -0,0 +1,21 @@
+import java.util.List;
+
+public class Tricount {
+    private String titre;
+    private List<String> devises;
+
+    public Tricount(String titre, List<String> devises) {
+        this.titre = titre;
+        this.devises = devises;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public List<String> getDevises() {
+        return devises;
+    }
+
+    
+}
